ed12703f2b19f97b320da12bb07b99120cf40939
arghaghosh <argha7417@gmail.com>
2023-10-21 18:40:51 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Navbar from './components/navbar';
+import Footer from './components/footer';
+
+function App() {
+  return (
+    <div>
+    <Navbar />
+    <h1>Upper body</h1>
+    <Footer />
+    </div>
+  )
+}
+
+export default App
