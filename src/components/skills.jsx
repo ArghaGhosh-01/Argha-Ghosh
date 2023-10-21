7a814ad849a533d48bd4495cd003a47cdb462adb
arghaghosh <argha7417@gmail.com>
2023-10-21 22:23:03 +0530

    Solved all the problems related to card

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -1,11 +1,18 @@
-function skills()
-{
-    return(
-        <div className="border-2 border-black mx-10 my-8 flex justify-center">
-            
-            
-
+import Card from "./skills_card"
+function skills() {
+    return (
+        <div class="mx-auto mt-10 w-auto">
+            <h2 class="text-2xl font-bold mb-4 flex justify-center">Technologies I know</h2>
+            <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mx-20 my-8">
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+            </div>
         </div>
+
     )
 }
 
