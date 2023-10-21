a96201a178d2e4caa4fb03f8938a393f753dcc32
arghaghosh <argha7417@gmail.com>
2023-10-21 21:50:58 +0530

    skills Sections created

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import Navbar from './components/navbar';
 import Footer from './components/footer';
 import Uppersection from './components/uppersection';
+import Skills from "./components/skills";
 
 function App() {
   return (
     <div>
     <Navbar />
     <Uppersection />
+    <Skills/>
     <Footer />
     </div>
   )
---
+++ src/components/skills.jsx
@@ -0,0 +1,12 @@
+function skills()
+{
+    return(
+        <div className="border-2 border-black mx-10 my-8 flex justify-center">
+            
+            
+
+        </div>
+    )
+}
+
+export default skills
