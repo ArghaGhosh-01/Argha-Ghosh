934fa089b52de81c8fddc872944444efcc820660
arghaghosh <argha7417@gmail.com>
2023-10-22 15:41:17 +0530

    Committed all changes

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -1,9 +1,9 @@
 import Card from "./skills_card"
 function skills() {
     return (
-        <div class="mx-auto mt-10 w-auto">
+        <div class="mx-10 mt-10 w-auto">
             <h2 class="text-2xl font-bold mb-4 flex justify-center">Technologies I know</h2>
-            <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mx-20 my-8">
+            <div class="max-w-screen-xl mx-auto mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
                 <Card />
                 <Card />
                 <Card />
