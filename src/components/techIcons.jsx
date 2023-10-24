a9913fbb22aa37493c002e687b19bce52ce3ad45
arghaghosh <argha7417@gmail.com>
2023-10-24 20:12:36 +0530

    Icons border added

--- src/components/techIcons.jsx
+++ src/components/techIcons.jsx
@@ -3,28 +3,28 @@ import React from 'react';
 const TechIcons = () => {
   return (
     <div className="flex flex-wrap items-center justify-center m-10">
-      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" className="h-10 m-2" alt="html5 logo" />
-      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" className="h-10 m-2" alt="css3 logo" />
-      <img src="https://cdn.simpleicons.org/tailwindcss/06B6D4" className="h-10 m-2" alt="tailwindcss logo" />
-      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg" className="h-10 m-2" alt="bootstrap logo" />
-      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" className="h-10 m-2" alt="javascript logo" />
-      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" className="h-10 m-2" alt="nodejs logo" />
-      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" className="h-10 m-2" alt="express logo" />
-      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg" className="h-10 m-2" alt="mongodb logo" />
-      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" className="h-10 m-2" alt="git logo" />
-      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" className="h-10 m-2" alt="github logo" />
-      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" className="h-10 m-2" alt="react logo" />
-      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/flutter/flutter-original.svg" className="h-10 m-2" alt="flutter logo" />
-      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/dart/dart-original.svg" className="h-10 m-2" alt="dart logo" />
-      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg" className="h-10 m-2" alt="java logo" />
-      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" className="h-10 m-2" alt="python logo" />
-      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-original.svg" className="h-10 m-2" alt="c logo" />
-      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg" className="h-10 m-2" alt="cplusplus logo" />
-      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jupyter/jupyter-original.svg" className="h-10 m-2" alt="jupyter logo" />
-      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/appwrite/appwrite-original.svg" className="h-10 m-2" alt="appwrite logo" />
-      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg" className="h-10 m-2" alt="figma logo" />
-      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg" className="h-10 m-2" alt="npm logo" />
-      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg" className="h-10 m-2" alt="linux logo" />
+      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" className="h-10 m-2 border border-white p-2 rounded-md" alt="html5 logo" />
+      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" className="h-10 m-2 border border-white p-2 rounded-md" alt="css3 logo" />
+      <img src="https://cdn.simpleicons.org/tailwindcss/06B6D4" className="h-10 m-2 border border-white p-2 rounded-md" alt="tailwindcss logo" />
+      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg" className="h-10 m-2 border border-white p-2 rounded-md" alt="bootstrap logo" />
+      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" className="h-10 m-2 border border-white p-2 rounded-md" alt="javascript logo" />
+      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" className="h-10 m-2 border border-white p-2 rounded-md" alt="nodejs logo" />
+      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" className="h-10 m-2 border border-white p-2 rounded-md" alt="express logo" />
+      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg" className="h-10 m-2 border border-white p-2 rounded-md" alt="mongodb logo" />
+      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" className="h-10 m-2 border border-white p-2 rounded-md" alt="git logo" />
+      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" className="h-10 m-2 border border-white p-2 rounded-md" alt="github logo" />
+      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" className="h-10 m-2 border border-white p-2 rounded-md" alt="react logo" />
+      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/flutter/flutter-original.svg" className="h-10 m-2 border border-white p-2 rounded-md" alt="flutter logo" />
+      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/dart/dart-original.svg" className="h-10 m-2 border border-white p-2 rounded-md" alt="dart logo" />
+      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg" className="h-10 m-2 border border-white p-2 rounded-md" alt="java logo" />
+      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" className="h-10 m-2 border border-white p-2 rounded-md" alt="python logo" />
+      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-original.svg" className="h-10 m-2 border border-white p-2 rounded-md" alt="c logo" />
+      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg" className="h-10 m-2 border border-white p-2 rounded-md" alt="cplusplus logo" />
+      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jupyter/jupyter-original.svg" className="h-10 m-2 border border-white p-2 rounded-md" alt="jupyter logo" />
+      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/appwrite/appwrite-original.svg" className="h-10 m-2 border border-white p-2 rounded-md" alt="appwrite logo" />
+      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg" className="h-10 m-2 border border-white p-2 rounded-md" alt="figma logo" />
+      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg" className="h-10 m-2 border border-white p-2 rounded-md" alt="npm logo" />
+      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg" className="h-10 m-2 border border-white p-2 rounded-md" alt="linux logo" />
     </div>
   );
 }
