de67f405aefb9ac98cce6be413ba85f8a9beaaa9
arghaghosh <argha7417@gmail.com>
2023-10-24 20:35:27 +0530

    Hover effect added to cards and icons

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -3,7 +3,9 @@ function skills() {
     return (
         <div class="mx-10 mt-10 w-auto">
             <h2 class="text-2xl font-bold mb-4 flex justify-center text-white">SKILLS</h2>
-            <div class="max-w-screen-xl mx-auto mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 justify-center items-center">
+            <div class="flex flex-wrap items-center justify-center">
+                <Card />
+                <Card />
                 <Card />
                 <Card />
      
