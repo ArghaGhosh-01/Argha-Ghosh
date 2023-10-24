b534f0bbc443513dfb45a09749e8d5e565f06f2f
arghaghosh <argha7417@gmail.com>
2023-10-24 22:03:20 +0530

    Changed the bg to stars

--- src/components/particlebg.jsx
+++ src/components/particlebg.jsx
@@ -11,124 +11,47 @@ function particlebg () {
     <Particles 
         id="tsparticles"
         init={loadParticles}
-        options={{
-            "fullScreen": {
-            "enable": true,
-            "zIndex": -1
+        options={{particles: {
+          number: {
+            value: 160,
+            density: {
+              enable: true,
+              value_area: 1500,
+            },
+          },
+          line_linked: {
+            enable: true,
+            opacity: 0.03,
+          },
+          move: {
+            direction: "right",
+            speed: 0.08,
+          },
+          size: {
+            value: 2.5,
+          },
+          opacity: {
+            anim: {
+              enable: true,
+              speed: 3,
+              opacity_min: 0.05,
+            },
+          },
         },
-        "particles": {
-            "number": {
-                "value": 10,
-                "density": {
-                    "enable": false,
-                    "value_area": 800
-                }
+        interactivity: {
+          events: {
+            onclick: {
+              enable: true,
+              mode: "push",
             },
-            "color": {
-                "value": "#fff"
+          },
+          modes: {
+            push: {
+              particles_nb: 1,
             },
-            "shape": {
-                "type": "star",
-                "options": {
-                    "sides": 5
-                }
-            },
-            "opacity": {
-                "value": 0.8,
-                "random": false,
-                "anim": {
-                    "enable": false,
-                    "speed": 1,
-                    "opacity_min": 0.1,
-                    "sync": false
-                }
-            },
-            "size": {
-                "value": 4,
-                "random": false,
-                "anim": {
-                    "enable": false,
-                    "speed": 40,
-                    "size_min": 0.1,
-                    "sync": false
-                }
-            },
-            "rotate": {
-                "value": 0,
-                "random": true,
-                "direction": "clockwise",
-                "animation": {
-                    "enable": true,
-                    "speed": 5,
-                    "sync": false
-                }
-            },
-            "line_linked": {
-                "enable": true,
-                "distance": 600,
-                "color": "#ffffff",
-                "opacity": 0.4,
-                "width": 2
-            },
-            "move": {
-                "enable": true,
-                "speed": 2,
-                "direction": "none",
-                "random": false,
-                "straight": false,
-                "out_mode": "out",
-                "attract": {
-                    "enable": false,
-                    "rotateX": 600,
-                    "rotateY": 1200
-                }
-            }
+          },
         },
-        "interactivity": {
-            "events": {
-                "onhover": {
-                    "enable": true,
-                    "mode": ["grab"]
-                },
-                "onclick": {
-                    "enable": false,
-                    "mode": "bubble"
-                },
-                "resize": true
-            },
-            "modes": {
-                "grab": {
-                    "distance": 400,
-                    "line_linked": {
-                        "opacity": 1
-                    }
-                },
-                "bubble": {
-                    "distance": 400,
-                    "size": 40,
-                    "duration": 2,
-                    "opacity": 8,
-                    "speed": 3
-                },
-                "repulse": {
-                    "distance": 200
-                },
-                "push": {
-                    "particles_nb": 4
-                },
-                "remove": {
-                    "particles_nb": 2
-                }
-            }
-        },
-        "retina_detect": true,
-        "background": {
-            "color": "#111",
-            "image": "",
-            "position": "50% 50%",
-            "repeat": "no-repeat",
-            "size": "cover"
-        }
+        retina_detect: true,
         }}
     />
   )
