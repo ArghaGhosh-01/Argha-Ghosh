8c2acbd146108c5e91d3c106011cd80d850e7b0e
arghaghosh <argha7417@gmail.com>
2023-10-27 14:13:57 +0530

    Skills data added

--- src/components/data/skillsData.js
+++ src/components/data/skillsData.js
@@ -1,7 +1,7 @@
 const skillsData = [
   { topic: "Frontend", status: "Intermediate", percentage: "70" },
   { topic: "Designing", status: "Advanced", percentage: "80" },
-  { topic: "WEB development", status: "Intermediate", percentage: "80" },
+  { topic: "WEB development", status: "Advanced", percentage: "80" },
   { topic: "APP development", status: "beginner", percentage: "40" },
 ];
 
