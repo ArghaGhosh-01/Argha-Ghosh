c4febf646d15b2eba41619e83f3c002e2d069034
arghaghosh <argha7417@gmail.com>
2023-10-27 14:35:07 +0530

    Data added to icons

--- src/components/data/iconsData.js
+++ src/components/data/iconsData.js
@@ -2,9 +2,9 @@ import GithubLogo from "./icons_img/github_icon.png";
 import Instagram from "./icons_img/instagram.png";
 
 const iconsData = [
+  { icon: GithubLogo, link: "https://github.com/argha7417" },
   { icon: GithubLogo, link: "https://github.com/yourusername" },
-  { icon: GithubLogo, link: "https://github.com/yourusername" },
-  { icon: Instagram, link: "https://www.instagram.com/yourusername/" },
+  { icon: Instagram, link: "https://www.instagram.com/argha_ghosh_143/" },
 ];
 
 export default iconsData;
