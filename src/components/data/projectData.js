1f313291a88c56d6b683f22b103a38b4ac02c878
arghaghosh <argha7417@gmail.com>
2023-11-06 18:13:09 +0530

    Project links added

--- src/components/data/projectData.js
+++ src/components/data/projectData.js
@@ -2,26 +2,26 @@ const projectData = [
     {
       name: "WEATHER APP",
       description: "Provides temperrature, cloudyness, humidity and many more after we enter the city name",
-      link: "google.com",
-      view: "okkk",
+      link: "https://github.com/ArghaGhosh-01/Weather-APP",
+      view: "https://github.com/ArghaGhosh-01/Weather-APP",
     },
     {
         name: " NEWS APP",
         description: "Kaisa hai?",
-        link: "google.com",
-        view: "okkk",
+        link: "https://github.com/ArghaGhosh-01/News-App",
+        view: "https://github.com/ArghaGhosh-01/News-App",
       },
       {
         name: "BOTIFY",
         description: "An AI powered chat application build using openai which can interact with the help of AI",
-        link: "https://github.com/argha7417/Botify",
-        view: "https://github.com/argha7417/Botify",
+        link: "https://github.com/ArghaGhosh-01/Botify",
+        view: "https://github.com/ArghaGhosh-01/Botify",
       },
       {
         name: "RANDOM PASSWORD GENERATOR",
         description: "Generates random password of custom lengths",
-        link: "https://github.com/Argha-Ghosh-01/Random-Password-Generator",
-        view: "https://argha-ghosh-01.github.io/Random-Password-Generator/",
+        link: "https://github.com/ArghaGhosh-01/Random-Password-Generator",
+        view: "https://github.com/ArghaGhosh-01/Random-Password-Generator",
       },
   ];
   
