6d0cfc07fa5c680aeae46f81caf22cefd0130331
arghaghosh <argha7417@gmail.com>
2023-11-08 01:08:10 +0530

    Radius added

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -5,11 +5,11 @@ import aboutData from "./data/aboutData";
 function about() {
   return (
     <div className="bg-black backdrop-blur-lg shadow-md mb-4 rounded-lg lg:mx-60 my-20 mx-16 text-white border border-white">
-      <div className="flex items-center justify-center text-xl font-mono font-bold text-white  mt-0 bg-slate-500 glow_card">
-        <span className="w-3 h-3 bg-red-500 rounded-full absolute left-11"></span>
-        <span className="w-3 h-3 bg-yellow-500 rounded-full absolute left-16"></span>
-        <span className="w-3 h-3 bg-green-500 rounded-full absolute left-20"></span>
-        ABOUT
+      <div className="flex items-center justify-center text-xl font-mono font-bold text-white  mt-0 bg-slate-500 glow_card rounded-md">
+        <span className="w-3 h-3 bg-red-500 rounded-full absolute left-4"></span>
+        <span className="w-3 h-3 bg-yellow-500 rounded-full absolute left-8"></span>
+        <span className="w-3 h-3 bg-green-500 rounded-full absolute left-12"></span>
+        TERMINAL
       </div>
       <div className="my-6">
         {aboutData.map((data, index) => (
--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -8,7 +8,7 @@ function Skills() {
       <h2 className="text-2xl font-bold mb-4 flex justify-center text-white font-mono">SKILLS</h2>
       <span className="bg-green-400 rounded-full w-96 h-96 opacity-10 absolute -left-20 circle"></span>
       <span className="bg-green-400 rounded-full w-60 h-60 opacity-10 absolute -right-0 -top-28 circle"></span>
-      <span className="bg-green-400 rounded-full w-96 h-96 opacity-10 absolute right-0 bottom-60 circle"></span>
+      <span className="bg-green-400 rounded-full w-96 h-96 opacity-10 absolute right-0 bottom-96 circle"></span>
       <div className="flex flex-wrap items-center justify-center ">
         {skillsData.map((data, index) => (
           <Card key={index} topic={data.topic} status={data.status} percentage={data.percentage} />
