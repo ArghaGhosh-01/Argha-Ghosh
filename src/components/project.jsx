eff349f91f49c2b0858f19122cb4b13efd42a59c
arghaghosh <argha7417@gmail.com>
2023-11-08 21:06:01 +0530

    Margin fixed

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -5,7 +5,7 @@ import projectData from "./data/projectData";
 function Project() {
   return (
     <div className="mx-20 w-auto py-5">
-      <h2 className="text-2xl font-sans font-bold mb-8 text-center text-white mx-10 mt-10 w-auto py-5">PROJECTS</h2>
+      <h2 className="text-2xl font-sans font-bold mb-8 text-center text-white mx-10 mt-10 w-auto py-2">PROJECTS</h2>
 
       <div className="flex flex-wrap -mx-4 justify-center lg:mx-20 md:mx-4">
         {projectData.map((data, index) => (
