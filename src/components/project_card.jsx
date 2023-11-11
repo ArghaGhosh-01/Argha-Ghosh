44ae7b4ac99c0eb67413f6f9962c10b2aab516f1
arghaghosh <argha7417@gmail.com>
2023-11-11 22:29:09 +0530

    Font color to white of ptoject name

--- src/components/project_card.jsx
+++ src/components/project_card.jsx
@@ -4,7 +4,7 @@ function ProjectCard({name,description,link,view}) {
   return (
     <div className=" w-full md:w-1/2 lg:w-1/3 px-4 mb-8">
       <div className="bg-opacity-50 backdrop-filter backdrop-blur-lg border border-white shadow-md rounded-lg p-6 card_glow">
-        <h3 className="text-xl font-mono font-bold mb-2 text-center text-green-300">{name}</h3>
+        <h3 className="text-xl font-mono font-bold mb-2 text-center text-gray-300">{name}</h3>
         <p className="text-white mb-4 text-center font-extralight text-sm">{description}</p>
         <div className="flex justify-center gap-2">
           <a href={link} target="_blank" rel="noopener noreferrer" className="text-white font-medium bg-green-600 border border-green-600 hover:bg-black hover:text-green-600  p-2 rounded-md ">Code</a>
