77c7a2b425d2269f1fc07a5eadda0474d5217f80
arghaghosh <argha7417@gmail.com>
2023-11-15 19:24:25 +0530

    changed the gap between tech icons

--- src/components/tech_icon_small.jsx
+++ src/components/tech_icon_small.jsx
@@ -20,7 +20,7 @@ function techiconsm({ image, name }) {
     <img
       ref={iconRef}
       src={image}
-      className={`h-10 m-2 border border-cyan-400 p-2 rounded-lg bg-opacity-50 backdrop-filter pointer-cursor backdrop-blur-lg icon_glow cursor-pointer ${isVisible ? 'fade-in' : 'fade-out'}`}
+      className={`h-10 m-4 border border-cyan-400 p-2 rounded-lg bg-opacity-50 backdrop-filter pointer-cursor backdrop-blur-lg icon_glow cursor-pointer ${isVisible ? 'fade-in' : 'fade-out'}`}
       alt={name}
     />
   );
