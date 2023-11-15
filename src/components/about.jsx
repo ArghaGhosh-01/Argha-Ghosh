78fc146e6ea08772636cdde06a341866f24e602f
arghaghosh <argha7417@gmail.com>
2023-11-15 10:56:14 +0530

    Final layout edit

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -17,7 +17,7 @@ function about() {
 
   return (
     <div ref={aboutRef} className={`backdrop-blur-lg shadow-md mb-4 rounded-lg lg:mx-60 my-20 mx-16 text-white card_glow ${isVisible ? 'fade-in' : 'fade-out'}`}>
-    <div className="flex items-center justify-center text-xl font-mono font-bold text-white  mt-0 bg-slate-500 glow_card rounded-md">
+    <div className="flex items-center justify-center text-xl font-mono font-bold text-white  mt-0 bg-slate-500 glow_card  rounded-tl-lg rounded-tr-lg rounded-br-md  rounded-bl-md">
         <span  className="w-3 h-3 bg-red-500 rounded-full absolute left-4"></span>
         <span className="w-3 h-3 bg-yellow-500 rounded-full absolute left-8"></span>
         <span className="w-3 h-3 bg-green-500 rounded-full absolute left-12"></span>
