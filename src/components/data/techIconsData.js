c90b4f691d98117bd7c21a539ed1a71a94068026
arghaghosh <argha7417@gmail.com>
2023-11-28 00:20:17 +0530

    Ions changed

--- src/components/data/techIconsData.js
+++ src/components/data/techIconsData.js
@@ -1,6 +1,6 @@
 import Github from "./icons_img/github_icon.png";
 import Express from "./icons_img/expressjs.png";
-import Vite from "./icons_img/vite.png";
+// import Vite from "./icons_img/vite.png";
 import Html from "./icons_img/html.png";
 import Css from "./icons_img/css.png";
 import Js from "./icons_img/js.png";
@@ -45,80 +45,7 @@ const techIconsData = [
     image:
       Js,
     name: "React",
-  },
-  {
-    image: Vite,
-    name: "Vite",
-  },
-  {
-    image:
-    Js,
-    name: "MongoDB",
-  },
-  {
-    image:
-    Js,
-    name: "Git",
-  },
-  {
-    image: Github,
-    name: "GitHub",
-  },
-  {
-    image:
-    Js,
-    name: "Flutter",
-  },
-  {
-    image:
-    Js,
-    name: "Dart",
-  },
-  {
-    image:
-    Js,
-    name: "Java",
-  },
-  {
-    image:
-    Js,
-    name: "Python",
-  },
-  {
-    image:
-    Js,
-    name: "C",
-  },
-  {
-    image:
-    Js,
-    name: "C++",
-  },
-  {
-    image:
-    Js,
-    name: "Jupyter",
-  },
-  {
-    image:
-    Js,
-    name: "Appwrite",
-  },
-  {
-    image:
-    Js,
-    name: "Figma",
-  },
-  {
-    image:
-    Js,
-    name: "npm",
-  },
-  {
-    image:
-    Js,
-    name: "Linux",
-  },
+  }
 ];
 
 export default techIconsData;
